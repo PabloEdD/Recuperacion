cdc14fccdefd63d4fba9155647a796999f248bef
PabloEdD <paorpablo@gmail.com>
2025-03-20 13:01:06 +0100

    Vistas Funcuonal

--- zelda/models/models.py
+++ zelda/models/models.py
@@ -1,19 +1,44 @@
 # -*- coding: utf-8 -*-
 
-# from odoo import models, fields, api
+from odoo import models, fields, api
 
+class jugador(models.Model):
+    _name = 'zelda.jugador'
+    _description = 'jugador'
 
-# class zelda(models.Model):
-#     _name = 'zelda.zelda'
-#     _description = 'zelda.zelda'
+    health = fields.Integer()
+    rupies = fields.Integer()
+    #objects = fields.One2many('zelda.objetos')
 
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         for record in self:
-#             record.value2 = float(record.value) / 100
+class personajes(models.Model):
+    _name = 'zelda.personajes'
+    _description = 'Personajes'
 
+    name = fields.Char(string="Name")
+
+class enemigos(models.Model):
+    _name = 'zelda.enemigos'
+    _description = 'Enemigos'
+
+    name = fields.Char(string="Name")
+    strength = fields.Integer()
+
+class objetos(models.Model):
+    _name = 'zelda.objetos'
+    _description = 'Objetos'
+
+    name = fields.Char(string="Name")
+    quantity = fields.Integer()
+    #player_id = fields.Many2one('zelda.jugador', string="Player")
+
+class pueblos(models.Model):
+    _name = 'zelda.pueblos'
+    _description = 'Pueblos'
+
+    name = fields.Char(string="Name")
+
+class mazmorras(models.Model):
+    _name = 'zelda.mazmorras'
+    _description = 'Mazmorras'
+
+    name = fields.Char(string="Name")
