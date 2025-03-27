e7cf19f48ae45577d27af4ffca5ae3d007c016eb
PabloEdD <paorpablo@gmail.com>
2025-03-27 13:16:30 +0100

    Vistas Funcional 2

--- zelda/models/models.py
+++ zelda/models/models.py
@@ -9,13 +9,19 @@ class jugador(models.Model):
     name = fields.Char(string="Name")
     rupies = fields.Integer()
     health = fields.Integer()
-    objects = fields.One2many('zelda.objetos', "player_id")
+    inventory = fields.One2many('zelda.inventario', "player_id")
+    objects = fields.Many2many('zelda.objetos', compute="_get_objects")
+
+    def _get_objects(self):
+        for a in self:
+            a.objects = a.inventory.objects
 
 class personajes(models.Model):
     _name = 'zelda.personajes'
     _description = 'Personajes'
 
     name = fields.Char(string="Name")
+    town = fields.Many2many('zelda.pueblos')
 
 class enemigos(models.Model):
     _name = 'zelda.enemigos'
@@ -24,20 +30,38 @@ class enemigos(models.Model):
     name = fields.Char(string="Name")
     strength = fields.Integer()
 
+class inventario(models.Model):
+    _name = 'zelda.inventario'
+    _description = 'Inventario'
+
+    name = fields.Char(string="Name")
+    player_id = fields.Many2one('zelda.jugador')
+    objects = fields.Many2one('zelda.objetos')
+
+
 class objetos(models.Model):
     _name = 'zelda.objetos'
     _description = 'Objetos'
 
     name = fields.Char(string="Name")
+    type = fields.Selection([('1','Bombas'),('2','Flechas')], )
     quantity = fields.Integer()
-    #type = fields.Selection(bombas, flehcas)
-    player_id = fields.Many2one('zelda.jugador', string="Name")
+
+
+class flechas(models.Model):
+    _name = 'zelda.flechas'
+    _description = 'Flechas'
+
+    name = fields.Char(string="Name")
+    type = fields.Selection([('1', 'Normal'), ('2', 'Fire'), ('3', 'Ice'), ('4', 'Electricity'), ('5', 'Ligth')], )
+    quantity = fields.Integer
 
 class pueblos(models.Model):
     _name = 'zelda.pueblos'
     _description = 'Pueblos'
 
     name = fields.Char(string="Name")
+    characters = fields.Many2many('zelda.personajes')
 
 class mazmorras(models.Model):
     _name = 'zelda.mazmorras'
